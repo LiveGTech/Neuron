834ac5e4d9bee3ac4dc42b5dfbd3c7894f7387c7
James-Livesey <jamesliveg@gmail.com>
2021-06-08 18:05:32 +0100

    Add base for development

---
+++ src/main.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+ 
+/*
+    Neuron
+  
+    Copyright (C) LiveG. All Rights Reserved.
+  
+    https://liveg.tech
+    Licensed by the LiveG Open-Source Licence, which can be found at LICENCE.md.
+*/
